c412b1cf61abd57a1c16e6c114ee159a6b1d9662
rudy klucik <rudyklucik@gmail.com>
2015-11-25 21:55:15 -0700

    misc updates, sprint

--- openlayersUpdate/antarctic-epsg3031.js
+++ openlayersUpdate/antarctic-epsg3031.js
@@ -1,24 +1,54 @@
 window.onload = function() {
 
-	proj4.defs("EPSG:3031",
-		"+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 " +
-		"+datum=WGS84 +units=m +no_defs");
+	proj4.defs("EPSG:3031", "+proj=stere +lat_0=-90 +lat_ts=-71 +lon_0=0 +k=1 +x_0=0 +y_0=0 +datum=WGS84 +units=m +no_defs");
 	ol.proj.get("EPSG:3031").setExtent([-4194304, -4194304, 4194304, 4194304]);
-
-	var map = new ol.Map({
+	// for outputting the mouse position
+	var mousePositionControl = new ol.control.MousePosition({
+		coordinateFormat: ol.coordinate.createStringXY(4),
+		projection: 'EPSG:4346',
+		// comment the following two lines to have the mouse position
+		// be placed within the map.
+		//className: 'custom-mouse-position',
+		//target: document.getElementById('mouse-position'),
+		undefinedHTML: '&nbsp;'
+	});
+	
+	map = new ol.Map({
+		controls: ol.control.defaults().extend([mousePositionControl]), // for displaying the mouse
 		view: new ol.View({
 			maxResolution: 8192.0,
 			projection: ol.proj.get("EPSG:3031"),
 			extent: [-4194304, -4194304, 4194304, 4194304],
 			center: [0, 0],
-			zoom: 0,
-			maxZoom: 7,
+			zoom: 0.5,
+			maxZoom: 5,
 		}),
 		target: "map",
-		renderer: ["canvas"],
+		renderer: ["canvas","dom"],
 	});
 
-	var sourceLandWater = new ol.source.WMTS({
+	// for drawing the box <http://openlayers.org/en/v3.8.2/examples/draw-features.html>
+	source = new ol.source.Vector({wrapX: false});
+	vector = new ol.layer.Vector({
+		source: source,
+		style: new ol.style.Style({
+			fill: new ol.style.Fill({
+				color: 'rgba(255, 255, 255, 0.2)'
+			}),
+			stroke: new ol.style.Stroke({
+				color: '#ffcc33',
+				width: 2
+			}),
+			image: new ol.style.Circle({
+				radius: 7,
+				fill: new ol.style.Fill({
+					color: '#ffcc33'
+				})
+			})
+		})
+	});
+	
+	sourceLandWater = new ol.source.WMTS({
 		url: "//map1{a-c}.vis.earthdata.nasa.gov/wmts-antarctic/wmts.cgi",
 		layer: "SCAR_Land_Water_Map", // jpeg 500m
 		extent: [-3923000, -3923000, 3923000, 3923000],//[-4194304, -4194304, 4194304, 4194304],
@@ -38,11 +68,11 @@ window.onload = function() {
 			tileSize: 512
 		})
 	});
-	var layerLandWater = new ol.layer.Tile({source: sourceLandWater});
+	layerLandWater = new ol.layer.Tile({source: sourceLandWater});
 	
 	// https://wiki.earthdata.nasa.gov/display/GIBS/GIBS+Available+Imagery+Products#expand-EarthatNight1Product
 	// coastline: SCAR antarctic digital database
-	var sourceBaseMap = new ol.source.WMTS({
+	sourceBaseMap = new ol.source.WMTS({
 		opacity: 0.5,
 		url: "//map1{a-c}.vis.earthdata.nasa.gov/wmts-antarctic/wmts.cgi",
 		//layer: "MODIS_Terra_CorrectedReflectance_TrueColor", // png 250m
@@ -64,9 +94,9 @@ window.onload = function() {
 			tileSize: 512
 		})
 	});
-	var layerBaseMap = new ol.layer.Tile({source: sourceBaseMap});
+	layerBaseMap = new ol.layer.Tile({source: sourceBaseMap});
 
-	var sourceCoastlines = new ol.source.WMTS({
+	sourceCoastlines = new ol.source.WMTS({
 		url: "//map1{a-c}.vis.earthdata.nasa.gov/wmts-antarctic/wmts.cgi",
 		layer: "Coastlines", // jpeg 500m
 		extent: [-4194304, -4194304, 4194304, 4194304],
@@ -86,10 +116,10 @@ window.onload = function() {
 			tileSize: 512
 		})
 	});
-	var layerCoastlines = new ol.layer.Tile({source: sourceCoastlines});
+	layerCoastlines = new ol.layer.Tile({source: sourceCoastlines});
 	
 	// Create an image layer, <http://www.acuriousanimal.com/thebookofopenlayers3/chapter02_04_image_layer.html>
-	var imageLayer = new ol.layer.Image({
+	imageLayer = new ol.layer.Image({
 		opacity: 0.7,
 		source: new ol.source.ImageStatic({
 			url: 'images/jan.png',
@@ -98,28 +128,63 @@ window.onload = function() {
 			imageExtent: [-4194304, -3923000, 4194304, 4194304]//[-3929786, -3929786, 3923000, 3923000]
 		})
 	});
-	//map.addLayer(imageLayer);
-	map.addLayer(layerLandWater);
-	//map.addLayer(layerBaseMap);
-	map.addLayer(layerCoastlines);
 
-	var heatmapLayer = new ol.layer.Heatmap({
+	heatmapLayer = new ol.layer.Heatmap({
 		source: new ol.source.GeoJSON({
 			url: 'cities.json',
 			projection: 'EPSG:3031'
 		}),
-		opacity: 0.9
+		opacity: 0.7
 	});
 	map.addLayer(heatmapLayer);
+
+	//map.addLayer(layerLandWater);
+	map.addLayer(layerBaseMap);
+	map.addLayer(layerCoastlines);
+	map.addLayer(imageLayer);
+
+	map.addLayer(vector);
 	
-	
-/*
-	// Change layer opacity
-	$('input.opacity').slider({
-		value: imageLayer.getOpacity()
-	}).on('slide', function(ev) {
-		imageLayer.setOpacity(ev.value);
-	});
-*/
+
+	typeSelect = document.getElementById('type');
+
+    //var draw; // global so we can remove it later
+      function addInteraction() {
+		
+		console.log("again");
+
+        var value = typeSelect.value;
+        if (value !== 'None') {
+          var geometryFunction, maxPoints;
+          if (value === 'Square') {
+            value = 'Circle';
+            geometryFunction = ol.interaction.Draw.createRegularPolygon(4);
+          } else if (value === 'Box') {
+            value = 'LineString';
+            maxPoints = 2;
+            geometryFunction = function(coordinates, geometry) {
+              if (!geometry) {
+                geometry = new ol.geom.Polygon(null);
+              }
+              var start = coordinates[0];
+              var end = coordinates[1];
+              geometry.setCoordinates([
+                [start, [start[0], end[1]], end, [end[0], start[1]], start]
+              ]);
+              return geometry;
+            };
+          }
+          draw = new ol.interaction.Draw({
+            source: source,
+            type: /** @type {ol.geom.GeometryType} */ (value),
+            geometryFunction: geometryFunction,
+            maxPoints: maxPoints
+          });
+          map.addInteraction(draw);
+        }
+      }
+
+
+	addInteraction();
 	
 };
